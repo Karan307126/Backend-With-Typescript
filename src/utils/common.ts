ff2e41dbaf8e1b3f2e2398370a48ceedfc55266d
KaranSarvaiya <karankumar.sarvaiya@tatvasoft.com>
2025-03-27 17:25:57 +0530

    handler functions are added

---
+++ src/utils/common.ts
@@ -0,0 +1,7 @@
+export const errorHandler = (error: unknown) => {
+    if (error instanceof Error) {
+        return error.message;
+    } else if (typeof error === "string") {
+        return error;
+    }
+};
